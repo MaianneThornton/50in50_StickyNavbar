ccb858ccd2c064c941da5ac9682a72f39184bedf
Maianne Thornton <maianne.thornton@gmail.com>
2022-07-30 15:57:34 -0400

    chore: spellcheck

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    // if the window scrolls down position is greater than the navbr offset height + 150 px
+    // if the window scrolls down position is greater than the navbar offset height + 150 px
     if (window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active')
     } else {
